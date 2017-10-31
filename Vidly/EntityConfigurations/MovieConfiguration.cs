d88f9369d5ecc8d5cbef6fdbe105ab3a88114f29
Manpreet <mpsingh17@outlook.co.nz>
2017-11-01 11:39:37 +1300

    Movies configuration added for fluent API.

---
+++ Vidly/EntityConfigurations/MovieConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Vidly.Models;
+
+namespace Vidly.EntityConfigurations
+{
+    public class MovieConfiguration : EntityTypeConfiguration<Movie>
+    {
+        public MovieConfiguration()
+        {
+            // Property Configuration.
+            Property(m => m.Name)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            
+        }
+    }
+}
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -10,8 +10,6 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
-        [Required]
-        [StringLength(255)]
         public string Name { get; set; }
 
         [Display(Name = "Release Date")]
